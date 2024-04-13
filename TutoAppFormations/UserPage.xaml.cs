a8d21b1bdb81ae8986dd7b275dd7b13e40d7ca09
Jeremy Lambrecq <Jeremy.lmbq@hotmail.com>
2024-04-13 11:05:36 +0200

    Page se connecter

--- TutoAppFormations/UserPage.xaml.cs
+++ TutoAppFormations/UserPage.xaml.cs
@@ -4,6 +4,17 @@ public partial class UserPage : ContentPage
 {
 	public UserPage()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        Uri uri = new Uri("https://www.google.com");
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new LoginPage());
+    }
 }
