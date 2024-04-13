e328f65762a4f4a24552079bade7eaf404faa41d
Jeremy Lambrecq <Jeremy.lmbq@hotmail.com>
2024-04-13 09:15:13 +0200

    a

---
+++ TutoAppFormations/Categorie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoAppFormations
+{
+    public class Categorie
+    {
+        public string Title { get; set; }
+
+        public int Count { get; set; }
+
+        public string Desc { get; set; }
+
+        public string Image { get; set; }
+
+        public Categorie(string title, int count, string desc, string image)
+        {
+            Title = title;
+            Count = count;
+            Desc = desc;
+            Image = image;
+        }
+    }
+}
--- TutoAppFormations/MainPage.xaml.cs
+++ TutoAppFormations/MainPage.xaml.cs
@@ -1,11 +1,30 @@
-﻿namespace TutoAppFormations
+﻿using Newtonsoft.Json;
+
+namespace TutoAppFormations
 {
     public partial class MainPage : ContentPage
     {
+        List<Categorie> categories = new List<Categorie>();
+
+        public List<Categorie> Categories
+        {
+            get
+            {
+                return categories;
+            }
+            set
+            {
+                categories = value;
+            }
+        }   
 
         public MainPage()
         {
             InitializeComponent();
+
+            //LoadDataFromAPI();
+
+            LoadDataFromLocalAsset();
         }
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -13,14 +32,23 @@
 
         }
 
-        public async void LoadDataFromAPI()
+        //public async void LoadDataFromAPI()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    //var RestURL = "http://localhost:5098/Categorie";
+        //    var RestURL = "http://10.0.2.2:5098/Categorie";
+        //    client.BaseAddress = new Uri(RestURL);
+        //    HttpResponseMessage response = await client.GetAsync(RestURL);
+        //    var content = await response.Content.ReadAsStringAsync();
+        //    var Items = JsonConvert.DeserializeObject<List<Categorie>>(content);
+        //    lv.ItemsSource = Items;
+        //}
+
+        public async void LoadDataFromLocalAsset()
         {
-            HttpClient client = new HttpClient();
-            //var RestURL = "http://localhost:5098/Categorie";
-            var RestURL = "http://10.0.2.2:5098/Categorie";
-            client.BaseAddress = new Uri(RestURL);
-            HttpResponseMessage response = await client.GetAsync(RestURL);
-            var content = await response.Content.ReadAsStringAsync();
+            using var stream = await FileSystem.OpenAppPackageFileAsync("Categories.json");
+            using var reader = new StreamReader(stream);
+            var content = reader.ReadToEnd();
             var Items = JsonConvert.DeserializeObject<List<Categorie>>(content);
             lv.ItemsSource = Items;
         }
