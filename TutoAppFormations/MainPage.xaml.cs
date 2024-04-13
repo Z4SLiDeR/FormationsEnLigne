803cce37289818e1d7a4ad5dcdcb306cfded6f0b
Jeremy Lambrecq <Jeremy.lmbq@hotmail.com>
2024-04-13 09:15:01 +0200

    Affichage list venant d'une API local + Affichage list json local

--- TutoAppFormations/MainPage.xaml.cs
+++ TutoAppFormations/MainPage.xaml.cs
@@ -12,6 +12,18 @@
         {
 
         }
+
+        public async void LoadDataFromAPI()
+        {
+            HttpClient client = new HttpClient();
+            //var RestURL = "http://localhost:5098/Categorie";
+            var RestURL = "http://10.0.2.2:5098/Categorie";
+            client.BaseAddress = new Uri(RestURL);
+            HttpResponseMessage response = await client.GetAsync(RestURL);
+            var content = await response.Content.ReadAsStringAsync();
+            var Items = JsonConvert.DeserializeObject<List<Categorie>>(content);
+            lv.ItemsSource = Items;
+        }
     }
 
 }
