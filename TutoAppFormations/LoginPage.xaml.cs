e7cce6544dba994912b04df88be501deab1a2a60
Jeremy Lambrecq <Jeremy.lmbq@hotmail.com>
2024-04-14 11:45:06 +0200

    Add button connection with error message

--- TutoAppFormations/LoginPage.xaml.cs
+++ TutoAppFormations/LoginPage.xaml.cs
@@ -7,8 +7,23 @@ public partial class LoginPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
+		//HttpClient client = new HttpClient();
+  //      var RestURL = "http://10.0.2.2:5098/Login?pseudo=" + pseudo.Text + "&pass=" + pass.Text;
+		//client.BaseAddress = new Uri(RestURL);
+		//HttpResponseMessage response = await client.GetAsync(RestURL);
+		//var content = await response.Content.ReadAsStringAsync(); 
+		
+		// dans le cas d'utilisation de l'API
 
+		if (pseudo.Text == "jeremy" && pass.Text == "1234")
+		{
+			await Navigation.PushAsync(new MainPage());
+		}
+		else
+		{
+			await DisplayAlert("Erreur", "Pseudo ou mot de passe incorrect", "OK");
+		}
     }
 }
